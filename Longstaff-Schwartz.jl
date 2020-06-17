d81045bd357153ff31841f46df7cabea56442d03
ashutosh-b-b <a.bharambe123@gmail.com>
2020-06-17 15:31:50 +0530

    Final Working Function

--- Longstaff-Schwartz.jl
+++ Longstaff-Schwartz.jl
@@ -1,38 +1,48 @@
 using LinearAlgebra , Polynomials , StochasticDiffEq
-N = 4          # number of time steps
-r = 0.06       # interest rate
-K = 1.1        # strike
-T = 3          # Maturity
+using Polynomials.PolyCompat , Distributions
 
-dt = T/(N-1)          # time interval
-df = exp(-r * dt)
+N = 4          # number of time steps
+K = 110.00        # strike
+T = 3.00          # Maturity
 M = 10
 S0 = fill(100.0 , M)
-r = 0.04
-sigma = 0.2
-f(u , p , t) = r
-g(u , p , t) = sigma
-prob = SDEProblem(f, g, S0 , (0.0 , 1.0))
-sol = solve(prob , EM() , dt = 1/N-1)
-for u in sol.u
-  S0 = hcat(S0 , u)
+r = 0.04         #interest_rate
+sigma = 0.2      #volatility
+
+function LSM_pricing(r , sigma, K , S0 , T , N , M)
+  dt = T/(N-1)          # time interval
+  df = exp(-r*dt)
+  f(u , p , t) = r.*u
+  g(u , p , t) = sigma.*u
+  prob = SDEProblem(f, g, S0 , (0.0 , T))
+  sol = solve(prob , EM() , dt = dt)
+  for u in sol.u
+    S0 = hcat(S0 , u)
+  end
+  S0 = S0[: ,2:end]
+  H = S0
+  for i in CartesianIndices(H)
+    H[i] = max(K - H[i] , 0.00)
+  end
+  V = zeros(size(H))
+  V[:,end] = H[:,end]
+  for t in N-1:-1:1
+    paths = H[:, t] .> 0
+    poly = Polynomials.fit(S0[paths , t] , V[paths , t + 1].* df, 2)
+    C = poly.(S0[paths , t])
+
+    exercise = fill(false , length(paths))
+    exercise[paths] = H[paths , t] .> C
+    V[exercise,t] = H[exercise,t]
+    for t_ in t+1:1:N-1
+      V[exercise , t_] .= 0
+    end
+    discount_path = (V[:,t] .== 0)
+
+    V[discount_path,t] = V[discount_path,t+1] * df
+  end
+  price = mean(V[: , 2])*df
+  return price
 end
-S0 = S0[: ,2:end]
-H = S0
-for i in CartesianIndices(H)
-  H[i] = max(K - H[i] , 0.00)
-end
-V = zeros(size(H))
-V[:,end] = H[:,end]
-for t in N-1:-1:1
-  paths = H[:, t] .> 0
-  poly = polyfit(S0[paths , t] , V[paths , t + 1], 2)
-  C = polyval(poly , S0[paths , t])
-  exc = fill(false , length(paths))
-  exercise[paths] = H[paths , t] .> C
-  V[exercise,t] = H[exercise,t]
-  V[exercise,t+1:] = 0
-  discount_path = (V[:,t] == 0)
-  V[discount_path,t] = V[discount_path,t+1] * df
-end
-price = mean(V[: , 2])*df
+
+LSM_pricing(r , sigma , K , S0 , T , N , M)
